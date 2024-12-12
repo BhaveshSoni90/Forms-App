8b37ba026890f7d86e4d0aaefa9419841a7d99d3
Bhavesh Soni <bhaveshbhama827@gmail.com>
2024-12-12 23:50:15 +0530

    new project's updates

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./App.js", // Ensure that the path points to your main component
+      "./components/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
